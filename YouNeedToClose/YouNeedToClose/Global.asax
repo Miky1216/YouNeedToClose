f8913c59cefa22a1258452f27e22e7cda8da8cd7
Mike <mike@mike.com>
2015-09-15 10:49:03 -0500

    Initial Commit

---
+++ YouNeedToClose/YouNeedToClose/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YouNeedToClose.MvcApplication" Language="C#" %>
---
+++ YouNeedToClose/YouNeedToClose/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YouNeedToClose.Startup))]
+namespace YouNeedToClose
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
