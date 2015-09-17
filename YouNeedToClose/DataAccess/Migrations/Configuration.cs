709ec7fef6f206bb10c8d4faddedd6e6a6c10889
Mike <mike@mike.com>
2015-09-17 15:50:50 -0500

    Round trip from database, controller, view functional and calculations fully functional

--- YouNeedToClose/DataAccess/Migrations/Configuration.cs
+++ YouNeedToClose/DataAccess/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace DataAccess.Migrations
 {
+    using DataAccess.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -26,6 +28,79 @@ namespace DataAccess.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            #region temporary sample data
+            TermModel term = new TermModel();
+            var startDate = new DateTime(2015, 9, 1);
+            var category = new List<CategoryModel>
+
+            {
+                new CategoryModel
+                {NameOfCategory="Reliable Customer", Customers = new List<CustomerModel>
+                    {
+                        new CustomerModel{NameOfCompany="GameStop", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=200
+                        }},
+                        new CustomerModel{NameOfCompany="BestBuy", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=200
+                        }},
+                        new CustomerModel{NameOfCompany="Target", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=200
+                        }}
+                    }
+                },
+                {
+                    new CategoryModel{NameOfCategory="Easy Sale Customer", Customers = new List<CustomerModel>{
+                        new CustomerModel{NameOfCompany="Kohls", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=200
+                        }},
+                        new CustomerModel{NameOfCompany="Costco", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=200
+                        }},
+                        new CustomerModel{NameOfCompany="WalMart", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=200
+                        }}
+                }
+            }},
+                {
+                    new CategoryModel{NameOfCategory="Unreliable Customer", Customers = new List<CustomerModel>{
+                        new CustomerModel{NameOfCompany="Woodmans", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=200
+                        }},
+                        new CustomerModel{NameOfCompany="Meijer", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=100
+                        }},
+                        new CustomerModel{NameOfCompany="PigglyWiggly", BudgetActualCustomer = new BudgetActualModel
+                        {
+                            Budget=800, Actual=1000, Difference=100
+                        }}
+                }
+            }}
+        };
+            term.StartDate = startDate;
+            term.Categories = category;
+            term.BudgetActual = new BudgetActualModel
+            {
+                Budget = 0,
+                Actual = 0,
+                Difference = 0
+            };
+
+            #endregion
+            
+            using (var db = new TermContext())
+            {
+                db.Term.Add(term);
+                db.SaveChanges();
+            }
         }
     }
 }
--- YouNeedToClose/YouNeedToClose/Controllers/HomeController.cs
+++ YouNeedToClose/YouNeedToClose/Controllers/HomeController.cs
@@ -45,7 +45,8 @@ namespace YouNeedToClose.Controllers
         }
         public ActionResult TermDisplay(int? id)
         {
-            TermModel term;
+            id = 1;
+            TermModel term = new TermModel();
             if (id == null)
             {
                 //determine which term is the current date
@@ -55,113 +56,45 @@ namespace YouNeedToClose.Controllers
             else
             {
                 //open term from database by id
-
-                //if term does not exist
-                //copy categories and budgetted from previous term from database
-
-            }
-
-
-            #region temporary sample data
-            term = new TermModel();
-            var startDate = new DateTime(2015, 9, 1);
-            var category = new List<CategoryModel>
-            
-
-            {
-                new CategoryModel
-                {NameOfCategory="Reliable Customer", Customers = new List<CustomerModel>
+                using (var db = new TermContext())
+                {
+                    term = db.Term.Find(id);
+                    term.BudgetActual = new BudgetActualModel
                     {
-                        new CustomerModel{NameOfCompany="GameStop", BudgetActualCustomer = new BudgetActualModel
+                        Budget = 0,
+                        Actual = 0,
+                        Difference = 0
+                    };
+                    foreach (CategoryModel termCategory in term.Categories)
+                    {
+                        termCategory.BudgetActualCategory = new BudgetActualModel
                         {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="BestBuy", BudgetActualCustomer = new BudgetActualModel
+                            Budget = 0,
+                            Actual = 0,
+                            Difference = 0
+                        };
+                        foreach (CustomerModel termCustomer in termCategory.Customers)
                         {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="Target", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }}
+                            termCustomer.BudgetActualCustomer.Difference = termCustomer.BudgetActualCustomer.Actual - termCustomer.BudgetActualCustomer.Budget;
+                            
+                            termCategory.BudgetActualCategory.Budget += termCustomer.BudgetActualCustomer.Budget;
+                            termCategory.BudgetActualCategory.Actual += termCustomer.BudgetActualCustomer.Actual;
+                            termCategory.BudgetActualCategory.Difference += termCustomer.BudgetActualCustomer.Difference;
+                            
+                        }
+                        term.BudgetActual.Budget += termCategory.BudgetActualCategory.Budget;
+                        term.BudgetActual.Actual += termCategory.BudgetActualCategory.Actual;
+                        term.BudgetActual.Difference += termCategory.BudgetActualCategory.Difference;
                     }
-                },
-                {
-                    new CategoryModel{NameOfCategory="Easy Sale Customer", Customers = new List<CustomerModel>{
-                        new CustomerModel{NameOfCompany="Kohls", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="Costco", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="WalMart", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }}
+                    var projectedMonthlyGoal = new ProjectedGoalModel
+                    {
+                        ExpectedAmountToEarn = 50000
+                    };
+                    //if term does not exist
+                    //copy categories and budgetted from previous term from database
                 }
-            }},
-                {
-                    new CategoryModel{NameOfCategory="Unreliable Customer", Customers = new List<CustomerModel>{
-                        new CustomerModel{NameOfCompany="Woodmans", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="Meijer", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=100
-                        }},
-                        new CustomerModel{NameOfCompany="PigglyWiggly", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=100
-                        }}
-                }
-            }}
-        };
-            term.StartDate = startDate;
-            term.Categories = category;
-            term.BudgetActual = new BudgetActualModel
-            {
-                Budget = 0,
-                Actual = 0,
-                Difference = 0
-            };
-
-            #endregion
-
-            foreach (CategoryModel termCategory in term.Categories)
-            {
-                termCategory.BudgetActualCategory = new BudgetActualModel
-                {
-                    Budget = 0,
-                    Actual = 0,
-                    Difference = 0
-                };
-                foreach (CustomerModel termCustomer in termCategory.Customers)
-                {
-                    termCategory.BudgetActualCategory.Budget += termCustomer.BudgetActualCustomer.Budget;
-                    termCategory.BudgetActualCategory.Actual += termCustomer.BudgetActualCustomer.Actual;
-                    termCategory.BudgetActualCategory.Difference += termCustomer.BudgetActualCustomer.Difference;
-                }
-                term.BudgetActual.Budget += termCategory.BudgetActualCategory.Budget;
-                term.BudgetActual.Actual += termCategory.BudgetActualCategory.Actual;
-                term.BudgetActual.Difference += termCategory.BudgetActualCategory.Difference;
             }
-
-            var budgetActual = new BudgetActualModel
-            {
-                Budget = 500,
-                Actual = 600,
-                Difference = 100
-            };
-
-            var projectedMonthlyGoal = new ProjectedGoalModel
-            {
-                ExpectedAmountToEarn = 50000
-            };
-
-            return View("TermView", term);
+           return View("TermView", term);
         }
     }
 }
