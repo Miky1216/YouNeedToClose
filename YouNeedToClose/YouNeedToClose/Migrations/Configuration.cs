6827328912402eb4f1af0cf5c788018bf0e505f8
Mike <mike@mike.com>
2015-09-28 15:51:30 -0500

    Got rid of seed data

--- YouNeedToClose/YouNeedToClose/Migrations/Configuration.cs
+++ YouNeedToClose/YouNeedToClose/Migrations/Configuration.cs
@@ -18,69 +18,7 @@ namespace YouNeedToClose.Migrations
         {
 
             TermModel term = new TermModel();
-            DetailsCustomerModel detailsCustomer = new DetailsCustomerModel();
             var startDate = new DateTime(2015, 9, 1);
-            var category = new List<CategoryModel>
-            {
-                new CategoryModel
-                {NameOfCategory="Reliable Customer", Customers = new List<CustomerModel>
-                    {
-                        new CustomerModel{NameOfCompany="GameStop", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="BestBuy", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="Target", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                    }
-                },
-                {
-                    new CategoryModel{NameOfCategory="Easy Sale Customer", Customers = new List<CustomerModel>{
-                        new CustomerModel{NameOfCompany="Kohls", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="Costco", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="WalMart", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }}
-                }
-            }},
-                {
-                    new CategoryModel{NameOfCategory="Unreliable Customer", Customers = new List<CustomerModel>{
-                        new CustomerModel{NameOfCompany="Woodmans", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=200
-                        }},
-                        new CustomerModel{NameOfCompany="Meijer", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=100
-                        }},
-                        new CustomerModel{NameOfCompany="PigglyWiggly", BudgetActualCustomer = new BudgetActualModel
-                        {
-                            Budget=800, Actual=1000, Difference=100
-                        }}
-                }
-            }},
-        };
-
-            term.StartDate = startDate;
-            term.Categories = category;
-            term.BudgetActual = new BudgetActualModel
-            {
-                Budget = 0,
-                Actual = 0,
-                Difference = 0
-            };
 
             using (var db = new TermContext())
             {
